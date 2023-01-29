1344d5d1a5ec0aac0c280c592cf83b9d05ffbd9f
Tarun <70534772+tarundevelops@users.noreply.github.com>
2023-01-29 17:11:22 +0530

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import Movie from "./Movie"
 import { useDispatch, useSelector, } from "react-redux"
 import { get } from "./dataSlice"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import axios from 'axios'
 import { useLoaderData } from "react-router-dom"
 
@@ -9,23 +9,41 @@ const Home = ()=>{
     const data = useSelector((state)=>state.data.value)
     const dispatch = useDispatch() 
     let st = useLoaderData() || "harry"
-    const [check,setCheck] = useState(false)
-    
+    const check = useRef(false)
+
     useEffect( ()=>{
-    
+    check.current=false
+    dispatch(get([]))
         axios.get(`https://www.omdbapi.com/?apikey=abaeb6be&s=${st}`).then((data)=>{
-            dispatch(get(data.data.Search))
-            if(data.data.Response == 'False') setCheck(true)
+            check.current=true
+                dispatch(get(data.data.Search))
+            
         })
         
     },[st])
-    return (
-        <div className="mDiv">
+
+    if(!check.current){
+        return (
+            <div style={{marginTop:"35vh",textAlign:"center"}}>
+
+                <div className="ld ld-ring ld-spin"
+     style={{fontSize:"64px"}}>
+</div>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }else{
+
+        return (
+            <div className="mDiv">
             {data? data.map((d)=>{
-               return <Movie info={d}/>
-            }): check ? "No Result Found":"Loading..."}
+                return <Movie info={d}/>
+            }):"No Result Found"}
+
         </div>
         
         )
+
+    }
 }
 export default Home
--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -5,10 +5,12 @@ import { useLoaderData } from "react-router-dom"
 const MovieDetail =()=>{
     const di = useLoaderData()
 
+    let wd = window.screen.width
+
 return (
     <div className="container pt-4">
     <div className="row">
-        <div className="col-8">
+        <div className={wd <= 758 ? "col-12" : "col-8"}>
             <h1>{di.Title}</h1>
             <p>{di.Plot}</p>
             <p>Director : {di.Director}</p>
@@ -16,7 +18,7 @@ return (
             <p>Writer : {di.Writer}</p>
             <p>IMDB : {di.imdbRating}</p>
         </div>
-        <div className="col-4">
+        <div className={wd <= 758 ? "col-12 p-4" : "col-4"}>
             <img src={di.Poster}/>
         </div>
     </div>
